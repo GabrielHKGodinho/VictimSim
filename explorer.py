c95d960f00570906893cd0593bb481d49375d961
misolani <88796761+misolani@users.noreply.github.com>
2023-09-20 23:48:16 -0300

    preencheMatriz

--- explorer.py
+++ explorer.py
@@ -28,18 +28,26 @@ class Explorer(AbstractAgent):
         # Criar uma matriz vazia
         self.mapa = []
 
+        # Armazena a posição do agente na matriz
+        self.xPos = int(self.rtime/2)
+        self.yPos = int(self.rtime/2)
+
+        # Posição da base
+        self.base = int(self.rtime/2)
+
         # Preencher a matriz com elementos (vamos usar 0 como exemplo)
         for _ in range(int(self.rtime)):
-            linha = [0] * int(self.rtime)
-            self.mapa.append(linha)   
-   
-    
+            linha = [-3] * int(self.rtime)
+            self.mapa.append(linha)
+
+        
+
     def deliberate(self) -> bool:
         """ The agent chooses the next action. The simulator calls this
         method at each cycle. Must be implemented in every agent"""
 
         # No more actions, time almost ended
-        if self.rtime < 10.0: 
+        if self.rtime < 1.0: 
             # time to wake up the rescuer
             # pass the walls and the victims (here, they're empty)
             print(f"{self.NAME} I believe I've remaining time of {self.rtime:.1f}")
@@ -59,6 +67,14 @@ class Explorer(AbstractAgent):
 
         # Moves the body to another position
         result = self.body.walk(dx, dy)
+        if result == 1:
+            self.preencheMapa()
+
+
+        # Atualiza a posição atual
+        self.xPos += dx
+        self.yPos += dy
+
 
         # Update remaining time
         if dx != 0 and dy != 0:
@@ -82,4 +98,46 @@ class Explorer(AbstractAgent):
                 # print(vs)
                 
         return True
+     
+    def preencheMapa(self):
+        loc =  [[0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]]
 
+        min = 100000
+        xMin = 0
+        yMin = 0
+
+        for y in [-1,0,1]:
+            for x in [-1, 0, 1]:
+                loc[y][x] = self.mapa[int(self.yPos) + y][int(self.xPos) +x]
+                if loc[y][x] < min and loc[y][x] > 0:
+                    xMin = y
+                    yMin = x
+                    min = loc[y][x]
+        
+        if min == 100000:
+            if self.xPos == len(self.mapa)//2 or self.yPos == len(self.mapa)//2:
+                self.mapa[self.yPos][self.xPos] = 1
+            else:
+                self.mapa[self.yPos][self.xPos] = 1.5
+
+        elif xMin == self.xPos or yMin == self.yPos:
+            self.mapa[self.yPos][self.xPos] = min + 1
+        else:
+            self.mapa[self.yPos][self.xPos] = min + 1.5
+
+        for y in range(int(self.rtime)):
+            for x in range(int(self.rtime)):
+                print(self.mapa[y][x], end= ' ')
+            print()
+        print()
+
+
+    
+        
+
+
+
+                
+        
